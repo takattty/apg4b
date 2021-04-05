e4041aa52b19e739a9e8eda79b275029bbf751fc
takattty <r9r_celtics_t9t@icloud.com>
2021-04-05 20:32:39 +0900

    done 1-3

---
+++ sample1/ex1-3.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  cout << 100 * (100 + 1) * 1 / 2 << endl;
+  // cout << 100 * (100 + 1) / 2 << endl; // これ参考コード
+}
---
+++ sample1/sample1-3.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  cout << 1 + 1 << endl; // 2
+  cout << 3 - 4 << endl; // -1
+  cout << 2 * 3 << endl; // 6
+  cout << 7 / 3 << endl; // 2 割り算は出来るだけ後ろの方で行うと正しい結果になる。整数同士の計算で切り捨てが発生するから。
+  cout << 7 % 3 << endl; // 1
+}
