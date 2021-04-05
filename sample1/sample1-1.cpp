91eee295492e467b1bed83b6b7a3671d39e4c4f7
takattty <r9r_celtics_t9t@icloud.com>
2021-04-05 19:45:46 +0900

    done 1-1

---
+++ sample1/sample1-1.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  cout << "Hello, world!" << endl; // endl=改行を表している。
+  cout << "a"; // ここ。改行がないから下のbと並んで出力される。
+  cout << "b" << endl;
+  cout << "c" << "d" << endl;
+  cout << 2525 << endl; // 数値はそのまま書けるぜ
+}
