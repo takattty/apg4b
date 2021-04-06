e5c2fbeb17bd1767434270622fc80bbb2a410fc5
takattty <r9r_celtics_t9t@icloud.com>
2021-04-06 13:47:41 +0900

    done 1-5

---
+++ sample1/sample1-5.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  // string text;
+  // double d;
+  // cin >> text;
+  // cin >> d;
+  // cout << text << ", " << d << endl;
+  int a, b, c;
+  cin >> a >> b >> c;
+  cout << a * b * c << endl;
+}
