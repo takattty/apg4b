c615c49796b0007e0b0a15bb180935259cd84495
takattty <r9r_celtics_t9t@icloud.com>
2021-04-29 00:57:57 +0900

    done 3-3

--- sample3/ex3-3.cpp
+++ sample3/ex3-3.cpp
@@ -1,28 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-  int N, A;
-  cin >> N;
-  map<int, int> res;
+int main(){
 
-  for(int i=0; i<N; i++) {
+  map<int,int> res;
+  int N, A, max=0, maxnum=-1;
+
+  cin >> N;
+  for(int i=0; i<N; i++){
     cin >> A;
-    if(res.count(A)) {
-      res[A] += 1;
-    } else {
+    if(res.count(A)){
+      res.at(A)++;
+    }else{
       res[A] = 1;
     }
   }
 
-  size_t count = 1;
-  for (auto p : res) {
-    auto k = p.first;
-    auto v = p.second;
-    count++;
-    if(count < res.size()) {
-      cout << k << " => " << v << endl;
+  for(auto p : res){
+    if(max < p.second){
+      max = p.second;
+      maxnum = p.first;
     }
   }
 
+  cout << maxnum << " " << max << endl;
 }
--- sample3/sample3-3.cpp
+++ sample3/sample3-3.cpp
@@ -110,3 +110,30 @@ int main() {
  
   cout << d.at(1) << endl; // 1
 }
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N, A;
+  cin >> N;
+  map<int, int> res;
+
+  for(int i=0; i<N; i++) {
+    cin >> A;
+    if(res.count(A)) {
+      res.at(A)++;
+    } else {
+      res[A] = 1;
+    }
+  }
+
+  size_t count = 1;
+  for (auto p : res) {
+    count++;
+    if(count > res.size()) {
+      cout << p.first << " " << p.second << endl;
+    }
+  }
+
+}
